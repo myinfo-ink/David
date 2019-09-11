06100161ccf37aeb7a1f172f30018008fe3f9ec9
DYXfollower <44867497+YiXTXFRC7095@users.noreply.github.com>
2019-09-11 14:27:37 +0800

    Delete index.js

--- js/index.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('h2').onmouseover = function() {
-    document.querySelector('#card').style.opacity = '1';
-}
-
-document.querySelector('h2').onmouseout = function() {
-    document.querySelector('#card').style.opacity = '0';
-}
